787a35e22cb827a85e0527487fa3c79f293b204c
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-02-12 12:41:32 +0530

    Time: 0 ms (100%), Space: 43.8 MB (50.52%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr[0].length;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length/2;j++){
+            int temp2=arr[i][j];
+            arr[i][j]=arr[i][arr[0].length-1-j];
+            arr[i][arr[0].length-1-j]=temp2;
+            }
+        }
+    }
+}
