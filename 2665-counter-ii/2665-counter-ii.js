fc28206c503ba34f7d4e84fdc90b77668ee15fdf
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-04-18 22:28:13 +0530

    Time: 46 ms (77.01%), Space: 57.3 MB (32.9%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count=init;
+    const increment=function(){
+        count++;
+        return count;
+    }
+    const reset=function(){
+        count=init;
+        return count;
+    }
+    const decrement=function(){
+        count--;
+        return count;
+    }
+    return{increment,reset,decrement};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
