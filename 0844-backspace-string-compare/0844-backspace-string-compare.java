833a281565a40d0cdd1b20bc1fe8cdeb0541fdae
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-05-17 14:52:42 +0530

    Time: 2 ms (56.96%), Space: 43.5 MB (13.94%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> st1=new Stack<>();
+        Stack<Character> st2=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch!='#'){
+                st1.push(ch);
+            }
+            else{
+                if(!st1.isEmpty()){
+                st1.pop();
+                }
+            }
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(ch!='#'){
+                st2.push(ch);
+            }
+            else{
+                if(!st2.isEmpty()){
+                    st2.pop();
+                }
+            }
+        }
+        return st1.equals(st2);
+    }
+}
