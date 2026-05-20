737d4925559b624d8d89ee267ebdf3fa209a884e
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-05-20 21:36:34 +0530

    Time: 19 ms (12.76%), Space: 47.5 MB (14.01%) - LeetHub

---
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        
+        int n=A.length;
+        int []C=new int[n];
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<=i;j++){
+                for(int k=0;k<=i;k++){
+                    if(A[j]==B[k]){
+                        count++;
+                    }
+                }
+            }
+            C[i]=count;
+        }
+        return C;
+    }
+}
