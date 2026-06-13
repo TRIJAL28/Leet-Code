60eb8456bdaf0635347ea527ebdeb055ace841bb
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-06-14 00:43:16 +0530

    Time: 1 ms (98.85%), Space: 46.2 MB (80.7%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        // if (nums.length==1){
+        //     return nums;
+        // }
+        int odd=0;
+        int even=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                even+=nums[i];
+            }
+            else{
+                odd+=nums[i];
+            }
+        }
+        return even-odd;
+    }
+}
