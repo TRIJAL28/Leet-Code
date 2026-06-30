927c118db69e5f200ec009f19643892ab605587f
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-06-30 18:30:11 +0530

    Time: 6 ms (36.98%), Space: 109.7 MB (5.91%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,23 +10,23 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode dummy=new ListNode(0);
-        ListNode copy=dummy;
+        ListNode dummy_node=new ListNode(-1);
+        ListNode dummy=dummy_node;
         ListNode temp=head;
         while(temp!=null){
-            copy.next=new ListNode(temp.val);
-            copy=copy.next;
+            dummy.next=new ListNode(temp.val);
+            dummy=dummy.next;
             temp=temp.next;
         }
         ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=null){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+        temp=head;
+        while(temp!=null){
+            ListNode next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
         }
-        ListNode first=dummy.next;
+        ListNode first=dummy_node.next;
         ListNode second=prev;
         while(first!=null && second!=null){
             if(first.val==second.val){
