0583f8a2cec24fa83bff60cd957e7f073ddeaf4d
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-07-03 22:20:29 +0530

    Time: 4 ms (38.72%), Space: 43.2 MB (72.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,18 +3,20 @@ class Solution {
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='['){
+            if(ch=='('||ch=='['||ch=='{'){
                 st.push(ch);
             }
-            else{
+            else{ 
                 if(st.isEmpty()){
                     return false;
                 }
-                else if(st.peek()=='(' && ch==')'|| st.peek()=='{' && ch=='}' || st.peek()=='[' && ch==']'){
-                    st.pop();
-                }
-                else{
-                    return false;
+                else if(!st.isEmpty()){
+                    if((st.peek()=='(' && ch==')')||(st.peek()=='{'&&ch=='}')||(st.peek()=='['&&ch==']')){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
                 }
             }
         }
