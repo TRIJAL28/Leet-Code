aa5d6a506137f27877da08fab72d235448d35122
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-07-04 23:18:38 +0530

    Time: 10 ms (56.84%), Space: 46.4 MB (31.16%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,35 +1,35 @@
-class Solution {
-    public int characterReplacement(String s, int k) {
-        s=s.toLowerCase();
-        int[] freq=new int[26];
-        char[] ch=new char[s.length()];
-        for(int i=0;i<s.length();i++){
-            ch[i]=s.charAt(i);
-        }
-        int left=0;
-        int right=0;
-        int maxlen=0;
-        int maxfreq=0;
-        int idxc=0;
-        while(right<ch.length){
-            int idx=ch[right]-'a';
-            if(freq[idx]==0){
-                idxc++;
+    class Solution {
+        public int characterReplacement(String s, int k) {
+            s=s.toLowerCase();
+            int[] freq=new int[26];
+            char[] ch=new char[s.length()];
+            for(int i=0;i<s.length();i++){
+                ch[i]=s.charAt(i);
             }
-            freq[idx]++;
-            maxfreq=Math.max(maxfreq,freq[idx]);
-            
-            while(((right-left+1)-maxfreq)>k){
-                int idx2=ch[left]-'a';
-                freq[idx2]--;
-                if(freq[idx2]==0){
-                    idxc--;
+            int left=0;
+            int right=0;
+            int maxlen=0;
+            int maxfreq=0;
+            int idxc=0;
+            while(right<ch.length){
+                int idx=ch[right]-'a';
+                if(freq[idx]==0){
+                    idxc++;
                 }
-                left++;
+                freq[idx]++;
+                maxfreq=Math.max(maxfreq,freq[idx]);
+                
+                while(((right-left+1)-maxfreq)>k){
+                    int idx2=ch[left]-'a';
+                    freq[idx2]--;
+                    // if(freq[idx2]==0){
+                    //     idxc--;
+                    // }
+                    left++;
+                }
+                maxlen=Math.max(maxlen,right-left+1);
+                right++;
             }
-            maxlen=Math.max(maxlen,right-left+1);
-            right++;
+            return maxlen;
         }
-        return maxlen;
     }
-}
