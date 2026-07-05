7b19e98723b3161aa997e4e606308eca4e97a4a3
TRIJAL SRIVASTAV <trijalsrivastav@gmail.com>
2026-07-05 23:10:32 +0530

    Time: 2641 ms (5.08%), Space: 51 MB (38.41%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        // int left=0;
+        // int right=0;
+        // int count=0;
+        // int sum=0;
+        // while(right<nums.length){
+        //     sum+=nums[right];
+        //     if(sum==goal){
+        //         count++;
+        //     }
+        //     while(sum>goal){
+        //         sum=sum-nums[left];
+        //         //nums[left]--;
+        //         left++;
+        //     }
+        //     right++;
+        // }
+        // return count;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            for(int j=i;j<nums.length;j++){
+               
+                sum+=nums[j];
+                if(sum==goal){
+                    count++;
+                }
+                
+            }
+        }
+        return count;
+    }
+
+}
