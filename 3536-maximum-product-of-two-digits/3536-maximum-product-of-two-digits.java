d1a8b038dba6f45820c04662b695cae272f19a01
Trijal Srivastav <trijalsrivastav@gmail.com>
2026-07-25 21:15:24 +0530

    Time: 2 ms (29.84%), Space: 42.8 MB (23.31%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -5,17 +5,17 @@ class Solution {
             list.add(n%10);
             n=n/10;
         }
-        // int max=0;
-        // int product=0;
-        // for(int i=0;i<list.size();i++){
-        //     for(int j=i+1;j<list.size();j++){
-        //         product=list.get(i)*list.get(j);
-        //         max=Math.max(max,product);
-        //     }
-        // }
-        // return max;
-        Collections.sort(list);
-        int product=list.get(list.size()-1)*list.get(list.size()-2);
-        return product;
+        int max=0;
+        int product=0;
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                product=list.get(i)*list.get(j);
+                max=Math.max(max,product);
+            }
+        }
+        return max;
+        // Collections.sort(list);
+        // int product=list.get(list.size()-1)*list.get(list.size()-2);
+        // return product;
     }
 }
