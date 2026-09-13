a301119606b74668b164c9de740cd593f327f213
Trijal Srivastav <trijalsrivastav@gmail.com>
2026-09-13 12:15:46 +0530

    Time: 0 ms (100%), Space: 43.2 MB (95.93%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        if(grid[0][0]==1) {return 0;}
+        else{
+            grid[0][0]=1;
+        }
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=1;i<m;i++){
+            if(grid[i][0]==1){
+                grid[i][0]=0;
+            }
+            else{
+                grid[i][0]=grid[i-1][0];
+            }
+        }
+        for(int j=1;j<n;j++){
+            if(grid[0][j]==1){
+                grid[0][j]=0;
+            }
+            else{
+                grid[0][j]=grid[0][j-1];
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(grid[i][j]==1){
+                    grid[i][j]=0;
+                }
+                else{
+                    grid[i][j]=grid[i-1][j]+grid[i][j-1];
+                }
+            }
+        }
+        return grid[m-1][n-1];
+    }
+}
